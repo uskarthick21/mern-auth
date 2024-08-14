8ed466d624becf753ead3753dc13cfbc4fc8b8eb
uskarthick21 <uskarthick21@gmail.com>
2024-08-14 12:15:13 +1000

    Error handlers and catch Error common function and http status code created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,34 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectToDatabase from "./config/db";
-import { NODE_ENV, PORT } from "./constants/env";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
+import errorHandler from "./middleware/errorHandler";
+import catchErrors from "./utils/catchErrors";
+import { OK } from "./constants/http";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.status(200).json({
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(
+    cors({
+        origin: APP_ORIGIN,
+        credentials: true,
+    })
+)
+app.use(cookieParser());
+
+app.get("/", (req, res, next) => {
+    return res.status(OK).json({
         status: "healty"
     })
 })
 
-app.listen(4004, async () => {
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
     console.log(`server is running on port ${PORT} in ${NODE_ENV} enviroment`);
     await connectToDatabase();
 })
